fdd5718cae019be7ef49c9bb880e1ab14bbdf410
amandapeixoto <amandapeixoto.ap1@gmail.com>
2023-09-18 21:40:03 -0300

    Atividade 2

--- app/src/main/java/com/amandapeixoto/loginapp/MainActivity.kt
+++ app/src/main/java/com/amandapeixoto/loginapp/MainActivity.kt
@@ -1,41 +1,60 @@
 package com.amandapeixoto.loginapp
 
-import android.annotation.SuppressLint
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var usernameEditText: EditText
     private lateinit var passwordEditText: EditText
-    private lateinit var loginButton: Button
-    private lateinit var welcomeTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val arquivo = getSharedPreferences("username_password", Context.MODE_PRIVATE)
+
+        val loginButton = findViewById<Button>(R.id.loginButton)
+        loginButton.setOnClickListener(this)
+
         usernameEditText = findViewById(R.id.usernameEditText)
         passwordEditText = findViewById(R.id.passwordEditText)
-        loginButton = findViewById(R.id.loginButton)
-        welcomeTextView = findViewById(R.id.welcomeTextView)
-
-        loginButton.setOnClickListener(View.OnClickListener {
-            val username = usernameEditText.text.toString()
-            val password = passwordEditText.text.toString()
-
-            if (username == "amanda" && password == "1234") {
-                val welcomeMessage = "Bem-vindo, $username!"
-                welcomeTextView.text = welcomeMessage
-            } else {
-                val errorMessage = "Nome de usuário ou senha incorretos."
-                welcomeTextView.text = errorMessage
-            }
-        })
-
     }
+
+    override fun onClick(view: View?) {
+        val arquivo = getSharedPreferences("username_password", Context.MODE_PRIVATE)
+        val username = arquivo.getString("login", "")
+        val password = arquivo.getString("senha", "")
+
+        if (username == "" && password == "") {
+            val editor = arquivo.edit()
+            editor.putString("login", usernameEditText.text.toString())
+            editor.putString("senha", passwordEditText.text.toString())
+            editor.commit()
+            Toast.makeText(
+                this,
+                "Bem vindo ao primeiro acesso!", //
+                Toast.LENGTH_LONG
+            ).show()
+        } else if ( usernameEditText.text.toString().equals(username) //
+            and passwordEditText.text.toString().equals(password) ) {
+            Toast.makeText(
+                this,
+                "Usuário e senha corretos!", //
+                Toast.LENGTH_LONG
+            ).show()
+        } else {
+            Toast.makeText(
+                this,
+                "Usuário e/ou senha inválidos!", //
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
 }
